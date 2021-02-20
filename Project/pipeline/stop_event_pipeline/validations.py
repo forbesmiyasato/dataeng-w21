c713cdae13af913773c848c82f272542eacc91ec
forbesmiyasato <miya5288@pacificu.edu>
2021-02-20 02:04:21 -0800

    producer and consumer for stop event

---
+++ Project/pipeline/stop_event_pipeline/validations.py
@@ -0,0 +1,59 @@
+def validateTripId(data):
+    key = 'trip_id'
+    if key not in data:
+        print("Trip ID doesn't exist in data.")
+        return False
+
+    id = data[key]
+
+    try:
+        id = int(data[key])
+    except ValueError:
+        print("The record has an abnormal trip ID of {}!".format(id))
+        return False
+
+    return id
+
+def validateServiceKey(data):
+    key = 'service_key'
+    if key not in data:
+        print("Service key doesn't exist in data.")
+        return False
+
+    service_key = data[key]
+    service_keys = {'W': 'Weekdays', 'S': 'Saturday'}
+
+    if service_key not in service_keys:
+        print("The record has an invalid service key of {}.".format(service_key))
+
+    return service_keys[service_key]
+
+
+def validateDirection(data):
+    key = 'direction'
+    if key not in data:
+        print("Direction doesn't exist in data.")
+        return False
+
+    direction = data[key]
+
+    if direction is not ['0','1']:
+        print("The record has an invalid direction of {}.".format(direction))
+
+    return direction
+
+def validateRouteId(data):
+    key = 'route_number'
+    if key not in data:
+        print("Route Id doesn't exist in data.")
+        return False
+
+    id = data[key]
+
+    try:
+        id = int(data[key])
+    except ValueError:
+        print("The record has an abnormal route ID of {}!".format(id))
+        return False
+
+    return id
